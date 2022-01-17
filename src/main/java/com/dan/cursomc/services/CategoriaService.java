a40350334f92bd6b833534df3f044ae25189cf2f
Dan Filipe <dfs@icomp.ufam.edu.br>
2022-01-17 13:49:38 -0400

    ajustes do endpoint

--- src/main/java/com/dan/cursomc/services/CategoriaService.java
+++ src/main/java/com/dan/cursomc/services/CategoriaService.java
@@ -2,6 +2,7 @@ package com.dan.cursomc.services;
 
 import com.dan.cursomc.domain.Categoria;
 import com.dan.cursomc.repositories.CategoriaRepository;
+import com.dan.cursomc.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,7 +16,7 @@ public class CategoriaService {
 
     public Categoria buscar(Integer id){
         Optional<Categoria> obj = repo.findById(id);
-        return  obj.orElseThrow(()-> new RuntimeException("id não encontrado"));
+        return  obj.orElseThrow(()-> new ObjectNotFoundException("Objeto não encontrado! ID: " + id +",Tipo :" + Categoria.class.getName()));
     }
 
 }
